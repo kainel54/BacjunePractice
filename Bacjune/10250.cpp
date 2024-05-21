96551e1ed1276fb1fdf82656fa1eae3a47c2ef94
kainel54 <kainel0504@gmail.com>
2024-05-21 23:57:23 +0900

    1157

--- Bacjune/10250.cpp
+++ Bacjune/10250.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-	int t,temp;
+	int t = 0, temp = 0;
 	cin >> t;
 
 	for (int i = 0;i < t;i++) {
@@ -10,17 +10,14 @@ int main() {
 		cin >> h >> w >> n;
 
 		if (n % h == 0) {
-			temp = h * 100;
+			temp = h * 100+n/h;
 		}
 		else {
-			temp = n % h * 100;
+			temp = n % h * 100+n/h+1;
 		}
-		if (n / h==0) {
-			temp += 1;
-		}
-		else {
-			temp += n / h+1;
-		}
+
+		
+
 		cout << temp<<endl;
 	}
 }
