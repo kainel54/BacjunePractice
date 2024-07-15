9452d25753bdd6229fe22783fd9cb82c4f3c770c
kainel54 <kainel0504@gmail.com>
2024-07-15 09:55:36 +0900

    4153

--- Bacjune/4153.cpp
+++ Bacjune/4153.cpp
@@ -5,34 +5,26 @@ using namespace std;
 
 int main() {
 	while (true) {
-		int a, b, c,temp,max;
+		int a, b, c,temp,max = 0;
 		cin >> a >> b >> c;
-		if (a > b) {
-			if (a > c) {
-				max == a;
-				temp = pow(b, 2) + pow(c, 2);
-			}
-			else {
-				max == c;
-				temp = pow(a, 2) + pow(b, 2);
-			}
-		}
-		else {
-			if (c > b) {
-				max == c;
-				temp = pow(a, 2) + pow(b, 2);
-			}
-			else {
-				max == b;
-				temp = pow(a, 2) + pow(c, 2);
-			}
-		}
-
 		if (a == 0 && b == 0 && c == 0) {
 			break;
 		}
 		
-		if (temp == pow(max, 2)) {
+
+		if (a > b) {
+			temp = b;
+			b = a;
+			a = temp;
+		}
+		if (b > c) {
+			temp = c;
+			c = b;
+			b = temp;
+		}
+
+		
+		if (pow(a,2)+pow(b,2) == pow(c,2)) {
 			cout << "right"<<endl;
 		}
 		else {
